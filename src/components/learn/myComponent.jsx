11ea630d9dd74986f31ea795061ae32cbf54ebf4
Kirito Yami <minhducnguyen.developer@gmail.com>
2024-12-31 11:49:49 +0700

    variable with JSX

--- src/components/learn/myComponent.jsx
+++ src/components/learn/myComponent.jsx
@@ -1,7 +1,17 @@
 const Function = () => {
+    // const a = "Hello World";
+    // const a = 25;
+    // const a = true;
+    // const a = undefined;
+    // const a = [1,2,3,4,5];
+    const a = {
+        name: 'Kirito Nguyen',
+        age: 27
+    }
     return (
         <>
-            <div>Kirito Nguyen</div>
+            <div>{JSON.stringify(a)} Kirito Nguyen</div>
+            <div>{console.log("Kirito, Ahihi")}</div>
             <div style={{borderRadius: "10px"}}>Tôi là 1 Developer</div>
         </>
     );
