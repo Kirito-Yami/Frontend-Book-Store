06a0739894a93a9adb5e7d669b9c9dba5b5f127d
Kirito Yami <minhducnguyen.developer@gmail.com>
2025-01-04 17:09:24 +0700

    Loading Bar

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
-import {
-    createBrowserRouter,
-    RouterProvider,
-} from "react-router-dom";
+import {createBrowserRouter, RouterProvider,} from "react-router-dom";
 import LoginPage from "./pages/login.jsx";
 import RegisterPage from "./pages/register.jsx";
 import UserPage from "./pages/user.jsx";
-import './styles/global.css';
 import TodoHome from "./components/todo/home.jsx";
 import {AuthWrapper} from "./components/context/auth.context.jsx";
 import BookPage from "./pages/book.jsx";
 import PrivateRoute from "./pages/private.route.jsx";
 import {ErrorPage} from "./pages/error.jsx";
+import 'nprogress/nprogress.css';
+import './styles/global.css';
 
 const router = createBrowserRouter([
     {
--- src/services/axios.customize.js
+++ src/services/axios.customize.js
@@ -1,4 +1,10 @@
 import axios from "axios";
+import NProgress from 'nprogress';
+
+NProgress.configure({
+    showSpinner: false,
+    trickleSpeed: 100,
+});
 
 // Set config defaults when creating the instance
 const instance = axios.create({
@@ -10,12 +16,14 @@ const instance = axios.create({
 
 // Add a request interceptor
 instance.interceptors.request.use(function (config) {
+    NProgress.start();
     if (typeof window !== "undefined" && window && window.localStorage && window.localStorage.getItem('access_token')) {
         config.headers.Authorization = 'Bearer ' + window.localStorage.getItem('access_token');
     }
     // Do something before request is sent
     return config;
 }, function (error) {
+    NProgress.done();
     // Do something with request error
     return Promise.reject(error);
 });
@@ -25,9 +33,11 @@ instance.interceptors.request.use(function (config) {
 instance.interceptors.response.use(function (response) {
     // Any status code that lie within the range of 2xx cause this function to trigger
     // Do something with response data
+    NProgress.done();
     if (response.data && response.data.data) return response.data;
     return response;
 }, function (error) {
+    NProgress.done();
     // Any status codes that falls outside the range of 2xx cause this function to trigger
     // Do something with response error
     if (error.response && error.response.data) return error.response.data;
