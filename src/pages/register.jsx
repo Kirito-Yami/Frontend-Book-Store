683297ac264b217757530f71570c28ef8bedc7eb
Kirito Yami <minhducnguyen.developer@gmail.com>
2025-01-04 08:50:37 +0700

    Complete feature Register

--- src/pages/register.jsx
+++ src/pages/register.jsx
@@ -1,10 +1,26 @@
-import { Button, Input, Form } from "antd";
+import {Button, Input, Form, notification} from "antd";
+import {registerUserAPI} from "../services/api.service.js";
+import {useNavigate} from "react-router-dom";
 
 const RegisterPage = ()=>{
     const [form] = Form.useForm();
+    const navigate = useNavigate();
 
-    const onFinish = (values) => {
-        console.log(">>> check values: ", values)
+    const onFinish = async (values) => {
+        const {fullName, email, password, phone} = values;
+        const res = await registerUserAPI(fullName, email, password, phone);
+        if(res.data){
+            notification.success({
+                message: "Register User",
+                description: "Đăng ký User thành công"
+            });
+            navigate("/login");
+        }else{
+            notification.error({
+                message: "Register User",
+                description: JSON.stringify(res.message)
+            });
+        }
     }
 
     return (
@@ -20,48 +36,48 @@ const RegisterPage = ()=>{
                 <Form.Item
                     label="Full Name"
                     name="fullName"
-                    // rules={[
-                    //     {
-                    //         required: true,
-                    //         message: 'Please input your username!',
-                    //     },
-                    // ]}
+                    rules={[
+                        {
+                            required: true,
+                            message: 'Please input your username!',
+                        },
+                    ]}
                 >
                     <Input />
                 </Form.Item>
                 <Form.Item
                     label="Email"
                     name="email"
-                    // rules={[
-                    //     {
-                    //         required: true,
-                    //         message: 'Please input your username!',
-                    //     },
-                    // ]}
+                    rules={[
+                        {
+                            required: true,
+                            message: 'Please input your username!',
+                        },
+                    ]}
                 >
                     <Input />
                 </Form.Item>
                 <Form.Item
                     label="Password"
                     name="password"
-                    // rules={[
-                    //     {
-                    //         required: true,
-                    //         message: 'Please input your username!',
-                    //     },
-                    // ]}
+                    rules={[
+                        {
+                            required: true,
+                            message: 'Please input your username!',
+                        },
+                    ]}
                 >
                     <Input.Password />
                 </Form.Item>
                 <Form.Item
                     label="Phone number"
                     name="phone"
-                    // rules={[
-                    //     {
-                    //         required: true,
-                    //         message: 'Please input your username!',
-                    //     },
-                    // ]}
+                    rules={[
+                        {
+                            required: true,
+                            message: 'Please input your username!',
+                        },
+                    ]}
                 >
                     <Input />
                 </Form.Item>
--- src/services/api.service.js
+++ src/services/api.service.js
@@ -32,6 +32,17 @@ const deleteUserAPI = (id) => {
     return axios.delete(URL_BACKEND);
 }
 
+const registerUserAPI = (fullName, email, password, phone) => {
+    const URL_BACKEND = "/api/v1/user/register";
+    const data = {
+        fullName: fullName,
+        email: email,
+        password: password,
+        phone: phone
+    }
+    return axios.post(URL_BACKEND, data);
+}
+
 const handleUploadFile = (file, folder) => {
     const URL_BACKEND = `/api/v1/file/upload`;
     let config = {
@@ -56,5 +67,6 @@ const updateUserAvatarAPI = (avatar, _id, fullName, phone) => {
 }
 
 export {
-    fetchAllUserAPI, createUserAPI, updateUserAPI, deleteUserAPI, handleUploadFile, updateUserAvatarAPI
+    fetchAllUserAPI, createUserAPI, updateUserAPI, deleteUserAPI,
+    registerUserAPI, handleUploadFile, updateUserAvatarAPI
 }
